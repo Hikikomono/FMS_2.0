2fb8b0e4ed980db733ccbc6595bfa09eb56230e7
Christoph Felber <faelb@gmx.at>
2020-01-07 15:02:59 +0100

    added parser for windows/linux

--- FMS/Parser.py
+++ FMS/Parser.py
@@ -9,27 +9,53 @@
     load the csv data and turn it into bookmark objects
 """
 
-
 import Caretaker
 import csv
+import os
+from sys import platform
+import getpass
 
 
 class Parser:
+    system = platform
+
     def __init__(self):
         self.carer = Caretaker.Caretaker()
 
     def get_bookmarks(self):
-        """this method crawls the csv created from the browser extension and returns a list of Bookmarks"""
-        # TODO: den Pfad dynamisch machen
-        with open('/home/faelb/Downloads/download') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=';')
-            line_count = 0
-            print("Log information for devs - folgende Bookmarks wurden aus dem csv in Bookmark Objekte verwandelt:")
-            for row in csv_reader:
-                # print(f'blabla {attribute} blabla') makes a formatted string
+        """this method crawls the csv created from the browser extension and returns a list of Bookmarks,depending on
+         the operating OS"""
+        user = getpass.getuser()
+        linuxpath = '/home/' + user + '/Downloads/download'
+        winpath = 'C:\\Users\\' + user + '\\Downloads\\Download'
 
-                self.carer.add_bookmark(None, row[1], row[0], row[3], None, [row[2]])
-
-                print(f'\t{row[0]} <-URL {row[1]} <-Title {row[2]} <-tags {row[3]} <-comment')
-                line_count += 1
-            print(f'Processed {line_count} lines.')
+        if platform == "linux" or platform == "linux2":
+            if os.path.exists(linuxpath):
+                with open(linuxpath) as csv_file:
+                    csv_reader = csv.reader(csv_file, delimiter=';')
+                    line_count = 0
+                    print("folgende Bookmarks wurden aus dem csv in Bookmark Objekte verwandelt:")
+                    for row in csv_reader:
+                        # print(f'blabla {attribute} blabla') makes a formatted string
+                        self.carer.add_bookmark(None, row[1], row[0], row[3], None, [row[2]])
+                        print(f'\t{row[0]} <-URL {row[1]} <-Title {row[2]} <-tags {row[3]} <-comment')
+                        line_count += 1
+                    print(f'Processed {line_count} lines.')
+                    os.remove(linuxpath)
+            else:
+                print("no file from plugin found")
+        elif platform == "win32":
+            if os.path.exists(winpath):
+                with open(winpath) as csv_file:
+                    csv_reader = csv.reader(csv_file, delimiter=';')
+                    line_count = 0
+                    print("folgende Bookmarks wurden aus dem csv in Bookmark Objekte verwandelt:")
+                    for row in csv_reader:
+                        # print(f'blabla {attribute} blabla') makes a formatted string
+                        self.carer.add_bookmark(None, row[1], row[0], row[3], None, [row[2]])
+                        print(f'\t{row[0]} <-URL {row[1]} <-Title {row[2]} <-tags {row[3]} <-comment')
+                        line_count += 1
+                    print(f'Processed {line_count} lines.')
+                    os.remove(winpath)
+            else:
+                print("no file from plugin found")
--- FMS/TestMain.py
+++ FMS/TestMain.py
@@ -4,6 +4,7 @@ from Caretaker import Caretaker
 from Bookmark import Bookmark
 from DbController import DbController
 from Parser import Parser
+from sys import platform
 
 test = DbController()
 test.init_tables()
@@ -11,7 +12,7 @@ test.init_tables()
 parser = Parser()
 carer = Caretaker()
 carer.get_list()
-parser.get_bookmarks()
+parser.get_bookmarks() #TODO: krieg ich diese method irnwie in den Caretaker? ws nicht weil sie eine instanz von caretaker aufruft
 # bookmark1 = Bookmark(1, "lala", "url", "comment","picture", ["list"])
 
 
@@ -20,7 +21,8 @@ carer.add_bookmark(None, "My second entry", "www.sex.lulu", "my comment222", Non
 carer.add_bookmark(None, "My 3rd entry", "www.lel.at", "my comment333", None, ["lulu", "gaga"])
 carer.add_bookmark(None, "My 4th entry", "www.4.at", "my comment444", None, ["istan", "bul", "rool", "woool", "swool"])
 
-
+test = platform
+print(test)
 
 # test deleting bookmark locally & from DB
 #carer.get_list()
